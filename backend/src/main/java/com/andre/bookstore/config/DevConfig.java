e6cd9291b30e3b1403e5a953a17485523edd1063
André Luis <andrelluis17@hotmail.com>
2021-11-11 16:20:12 -0300

    criacao do perfil de dev

---
+++ backend/src/main/java/com/andre/bookstore/config/DevConfig.java
@@ -0,0 +1,29 @@
+package com.andre.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.andre.bookstore.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instanciaBaseDeDados() {
+
+		if (strategy.equalsIgnoreCase("create")) {
+			this.dbService.instaciaBaseDeDados();
+		}
+		return false;
+	}
+}
