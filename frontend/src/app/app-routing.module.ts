01c04eb4247431e6ec99311386e01b64f9df5aaf
André Luis <andrelluis17@hotmail.com>
2021-11-16 16:16:36 -0300

    Component Categoria-read implementado

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/views/home/home.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { CategoriaReadComponent } from "./components/views/categoria-read/categoria-read.component";
+import { HomeComponent } from "./components/views/home/home.component";
 
 const routes: Routes = [
   {
-    path: '',
-    component: HomeComponent
-  }
+    path: "",
+    component: HomeComponent,
+  },
+  {
+    path: "categorias",
+    component: CategoriaReadComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ frontend/src/app/components/views/categoria-read/categoria-read.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from "@angular/core";
+import { Categoria } from "./categoria.model";
+import { CategoriaService } from "./categoria.service";
+
+@Component({
+  selector: "app-categoria-read",
+  templateUrl: "./categoria-read.component.html",
+  styleUrls: ["./categoria-read.component.css"],
+})
+export class CategoriaReadComponent implements OnInit {
+  categorias: Categoria[] = [];
+
+  displayedColumns: string[] = ["id", "name", "description", "action"];
+
+  constructor(private service: CategoriaService) {}
+
+  ngOnInit(): void {
+    this.findall();
+  }
+
+  findall() {
+    this.service.findall().subscribe((resposta) => {
+      console.log(resposta);
+      this.categorias = resposta;
+    });
+  }
+}
